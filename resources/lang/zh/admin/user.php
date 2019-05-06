ac514dbd747c522158a4dea32afc8205804da1e5
1079805974 <1079805974@qq.com>
2019-05-06 19:45:24 +0800

    first commit

---
+++ resources/lang/zh/admin/user.php
@@ -0,0 +1,25 @@
+<?php
+return [
+	'title' 	=> '学生管理',
+	'desc' 		=> '学生列表',
+	'create' 	=> '添加学生',
+	'edit' 		=> '修改学生',
+	'info' 		=> '学生信息',
+	'permission'=> '权限',
+	'role'		=> '角色',
+	'module'	=> '模块',
+	'model' 	=> [
+		'id' 			=> 'ID',
+		'name' 			=> '姓名',
+		'username' 		=> '账号',
+		'password' 		=> '密码',
+		'email' 		=> '邮箱',
+        'created_at' 	=> '创建时间',
+        'updated_at' 	=> '修改时间',
+	],
+	'action' => [
+		'create' => '<i class="fa fa-user"></i> 添加学生',
+	],
+	'other_permission'	=> '<strong>注意！</strong> 当某个角色的学生需要额外权限时添加。',
+	'role_info'			=> '查看角色权限',
+];
